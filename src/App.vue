1d555bbd3c4776f645c60a5e2633b7a316bb9a2d
Thea Mushambadze <thea.mushambadze@gmail.com>
2023-04-13 18:39:02 +0400

    1st commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script>
+  import TheNavigation from '@/components/TheNavigation.vue'
+  export default {
+    components: {TheNavigation}
+  }
+</script>
+
+<template>
+  <TheNavigation />
+  
+</template>
---
+++ src/components/TheNavigation.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="nav">
+        <router-link to="/">Home</router-link>
+        <router-link to="/about">My Destinations</router-link>
+        
+    </div>
+    <div class="container">
+        <router-view></router-view>
+    </div>
+</template>
+
+<style lang="css">
+#nav .active {
+   color: #fff;
+   opacity: 1;
+}
+</style>
